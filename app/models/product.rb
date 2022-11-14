d4250dfacc31da351f53082e10bb88dd7cbe5859
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-11-14 19:17:31 +0900

    #6 productモデル 修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
-  belongs_to :product, dependent: :destroy
+  belongs_to :user
+  belongs_to :category
+  belongs_to :sale_status
+  belongs_to :product_status
+  has_many :purchases, dependent: :destroy
 end
