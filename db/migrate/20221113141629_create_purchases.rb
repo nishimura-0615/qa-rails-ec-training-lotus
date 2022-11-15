cbfe25ec23ac9b5f4fec5aad88d0577a1da9b9f7
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-11-15 10:13:09 +0900

    #6 nullを許容しない形に修正

--- db/migrate/20221113141629_create_purchases.rb
+++ db/migrate/20221113141629_create_purchases.rb
@@ -1,10 +1,10 @@
 class CreatePurchases < ActiveRecord::Migration[7.0]
   def change
     create_table :purchases do |t|
-      t.integer :purchase_price
-      t.integer :product_quantity
-      t.string :purchase_company, :limit => 128
-      t.datetime :order_date
+      t.integer :purchase_price, null: false
+      t.integer :product_quantity, null: false
+      t.string :purchase_company, null: false, :limit => 128
+      t.datetime :order_date, null: false
       t.datetime :purchase_date
       t.references :product, null: false, foreign_key: true
       t.timestamps
