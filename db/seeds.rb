f773d97885ce78676c0376aaa7cb397acf9053e8
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-11-22 01:18:50 +0900

    #9 ドメインや波括弧やコロン記述を再度修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user_classification = UserClassification.create!(user_classification_name:"一般ユーザー")
+user_classification = UserClassification.create!(user_classification_name: "一般ユーザー")
 User.create!([{ email: "user001@example.com", password: "password1234", first_name: "user", last_name: "001",
              user_classification_id: user_classification.id, zipcode: "123-4567", prefecture: "東京都", municipality: "千代田区", address: "代々木1丁目31-1",
              apartments: "代々木ビル1F", phone_number: "0366192257", company_name: "株式会社 清水塾" },
