b60876c97448feb23d6f4dbf8b2e471b42c7dfa7
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-11-30 20:45:10 +0900

    #14 ルーティングをresourcesに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users/:id', to: 'users#show'
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
