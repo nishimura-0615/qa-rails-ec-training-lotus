c58ace42cf3d3d2575788f117a0c52e36be043f7
shinohara <>
2022-12-03 23:04:27 +0900

    #19 ユーザー情報修正画面をform_withに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
-  get 'users/:id/edit', to: 'users#edit'
   # Defines the root path route ("/")
   # root "articles#index"
 end
