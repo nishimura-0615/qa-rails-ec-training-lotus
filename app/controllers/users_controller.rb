0552f45ec6b2225b200c5c401906a1c712281c12
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-12-11 20:15:44 +0900

    #22 ユーザー情報更新機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find_by(id: params[:id])
   end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update!(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ユーザー情報の更新に失敗しました。"
+      render "users/edit"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_digest)
+    end
 end
