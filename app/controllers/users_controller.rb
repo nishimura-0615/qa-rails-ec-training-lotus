82ad2c57809435bdfbc9d08b31c06c54c8f27b6a
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-12-15 17:43:24 +0900

    #22 permitの記述を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_digest)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email,:zipcode , :prefecture, :address, :municipality, :apartments, :phone_number, :password_digest, :company_name, :user_classification_id)
+  end
 end
