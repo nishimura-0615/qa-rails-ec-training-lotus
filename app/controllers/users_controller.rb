9987c2e05f4cfc829f8711dedf10fdb0d5f35323
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-12-16 18:03:29 +0900

    #20 rubocopを実行し、自動で記述を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :zipcode, :password, :prefecture, :address, :municipality, :apartments, :phone_number, :password_digest, :company_name, :user_classification_id)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :zipcode, :password, :prefecture, :address, :municipality, :apartments, :phone_number, :password_digest,
+                                   :company_name, :user_classification_id)
+    end
 end
