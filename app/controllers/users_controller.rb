8374f6e34eded10cda06e49a009ccf7825b775c1
shinohara <>
2022-12-18 10:00:57 +0900

    #23 ログイン画面の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only:[:edit, :update, :destroy]
+
   def show
     @user = User.find_by(id: params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
   resources :products
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
 end
