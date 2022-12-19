15586506a5e0a9fbe1fad20f342441b23fbfb6b8
shinohara <>
2022-12-19 10:21:02 +0900

    routes.rbにas: 'login'を追記

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
   resources :products
-  get    '/login',   to: 'sessions#new'
+  get    '/login',   to: 'sessions#new', as: 'login'
   # Defines the root path route ("/")
   # root "articles#index"
 end
