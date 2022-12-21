5f7ff2f9088acdf926b41ae497db579b57253a42
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-12-21 23:32:59 +0900

    #20 商品の合計や関連の表示を修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
+
+  def total_price
+    sub_totals = []
+    order_details.each do |order_detail|
+      sub_totals << order_detail.sub_total
+    end
+    sub_totals.sum
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,4 +4,8 @@ class OrderDetail < ApplicationRecord
   belongs_to :shipment_status
 
   validates :order_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
+  def sub_total
+    product.price * order_quantity
+  end
 end
