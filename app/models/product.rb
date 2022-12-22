bf846a5dcc4610ad058632483eb3748bf025cee2
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-12-22 22:46:57 +0900

    #20 product.rbにアソシエーションを追記

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :sale_status
   belongs_to :product_status
+  has_many :order_details, dependent: :nullify
   has_many :purchases, dependent: :destroy
 end
