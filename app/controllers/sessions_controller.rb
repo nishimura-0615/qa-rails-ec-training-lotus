70691ca90600c7d563f89cbe7a1e879a63e71760
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-12-25 21:04:07 +0900

    #24 ログイン、ログアウト処理エラー発生中

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to "users/show"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to log_in
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/login', to: 'sessions#new', as: 'login'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy', as: 'logout'
   resources :users
   resources :products
   resources :orders
