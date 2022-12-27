984465dfcb31cfacffefc763a40c1fbd11d13669
nishimura-0615 <nsmr0615kenta@gmail.com>
2022-12-27 21:07:36 +0900

    #24 routes.rbの/logoutのアクションの記述をdeleteに修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/login', to: 'sessions#new', as: 'login'
   post   '/login',   to: 'sessions#create'
-  get '/logout',  to: 'sessions#destroy', as: 'logout'
+  delete '/logout',  to: 'sessions#destroy', as: 'logout'
   resources :users
   resources :products
   resources :orders
