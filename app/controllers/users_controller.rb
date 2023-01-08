0b0909653373d5475901c6c1a465080dcea7c907
nishimura-0615 <nsmr0615kenta@gmail.com>
2023-01-08 23:09:04 +0900

    #29 ユーザー登録ページ、登録処理を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,24 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
   def edit
     @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:successful] = I18n.t("activerecord.errors.models.user.message.create.successful")
+      redirect_to log_in
+    else
+      flash[:failed] = I18n.t("activerecord.errors.models.user.message.create.failed")
+      render "users/new"
+    end
   end
 
   def update
@@ -21,7 +37,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :zipcode, :password, :prefecture, :address, :municipality, :apartments, :phone_number, :password_digest,
-                                   :company_name, :user_classification_id)
+      params.permit(:first_name, :last_name, :email, :zipcode, :password, :prefecture, :address, :municipality, :apartments, :phone_number, :password_digest,
+                    :company_name, :user_classification_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: 'login'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy', as: 'logout'
+  get 'signup', to:'users#new'
+  post 'signup', to:'users#create'
   resources :users
   resources :products
   resources :orders
