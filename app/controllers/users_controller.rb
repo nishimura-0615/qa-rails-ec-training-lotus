4750b1f2eeb167cd5de4cea32465e29eed79905c
nishimura-0615 <nsmr0615kenta@gmail.com>
2023-01-11 22:17:41 +0900

    #29 ユーザー登録処理ができる様に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.user_classification_id =1
     if @user.save
       session[:user_id] = @user.id
       flash[:successful] = I18n.t("activerecord.errors.models.user.message.create.successful")
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.permit(:first_name, :last_name, :email, :zipcode, :password, :prefecture, :address, :municipality, :apartments, :phone_number, :password_digest,
+      params.require(:user).permit(:first_name, :last_name, :email, :zipcode, :password, :prefecture, :address, :municipality, :apartments, :phone_number, :password_digest,
                     :company_name, :user_classification_id)
     end
 end
