24fc8589875dce04e1bcb6b0b20a80466dbf96cb
nishimura-0615 <nsmr0615kenta@gmail.com>
2023-01-13 17:13:02 +0900

    #31  csrfのエラーを解消するために記述を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-
-  def set_csrf_token_header
-    binding.pry
-    response.set_header('X-CSRF-Token', form_authenticity_token)
-  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,6 @@ module SessionsHelper
   end
 
   def current_user
-    binding.pry
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
