d6b499c8d3b5627c8c8de9a35a92989187d23374
nishimura-0615 <nsmr0615kenta@gmail.com>
2023-01-18 00:52:20 +0900

    #34 注文キャンセル機能を実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,10 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
   end
+
+  def destroy
+    @order = Order.find_by(id: params[:id]).destroy!
+    # TODO:注文履歴ページの実装時に対応したリンクにリダイレクトする様に修正
+    redirect_to order_path
+  end
 end
